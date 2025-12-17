c7d3d12b41ad2909f28856cddecc268237db1f7c
charanaerpula23-boop <charanaerpula23@gmail.com>
2025-12-17 14:07:46 +0530

    Add Render.com deployment configuration

--- flaskapp.py
+++ flaskapp.py
@@ -139,4 +139,6 @@ def healthz():
 
 if __name__ == "__main__":
     # Run local dev server
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
